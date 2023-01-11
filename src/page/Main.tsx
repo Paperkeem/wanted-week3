de518fb17285472fbd87fe93370f944e222a2371
Paperkeem <ahharlawhddl@gmail.com>
2023-01-11 18:29:43 +0900

    chore: 개발 환경 설정

---
+++ src/page/Main.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Recommend from '../components/Recommend';
+import Search from '../components/Search';
+
+export default function Main() {
+  return (
+    <main>
+      <Search />
+      <Recommend />
+    </main>
+  );
+}
+
